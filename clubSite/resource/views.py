e7a0a67ec4ff811058766dbee51311447e7450ab
AW-AlanWu <alan.92925@gmail.com>
2021-07-28 22:40:05 +0800

    Add button in problem

--- clubSite/resource/views.py
+++ clubSite/resource/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 def problem(request):
     context = {}
+    context['order_list'] = range(185)
     if request.user.is_authenticated:
         context['is_authenticated'] = True
     return render(request, 'resource/problem.html', context=context)
