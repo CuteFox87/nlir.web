2fd678a249380d29f3cd7c745ab42b672cce140d
AW-AlanWu <alan.92925@gmail.com>
2021-07-28 15:30:05 +0800

    Add a for-loop in display

--- clubSite/display/views.py
+++ clubSite/display/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 def display(request):
     context = {}
+    context['list'] = range(10)
     if request.user.is_authenticated:
         context['is_authenticated'] = True
     return render(request, 'display/display.html', context=context)
