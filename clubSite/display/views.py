ec423decce543e8ff4f7101f7eb7498880a62601
AW-AlanWu <alan.92925@gmail.com>
2021-07-28 15:53:42 +0800

    Complete the display module

--- clubSite/display/views.py
+++ clubSite/display/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Project
 
 def display(request):
-    context = {}
-    context['list'] = range(10)
+    context = {
+        'project_list' : Project.objects.order_by('-pk')
+    }
     if request.user.is_authenticated:
         context['is_authenticated'] = True
     return render(request, 'display/display.html', context=context)
