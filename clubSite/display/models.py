b3af8805d59a64a9aa5ac7f911c56483c97d2e87
AW-AlanWu <alan.92925@gmail.com>
2021-07-28 15:42:39 +0800

    Add 'Project' model

--- clubSite/display/models.py
+++ clubSite/display/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Project(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        default=None
+    )
+    project_name = models.CharField(u"專案名稱",max_length=20)
+    description = models.CharField(u"敘述",max_length=600)
+    link = models.CharField(u"連結",max_length=200)
+    date = models.DateTimeField('發表日期', null=True)
+
+    def __str__(self):
+        return self.project_name
+
+    @property
+    def showAuthor(self):
+        return self.user.username
