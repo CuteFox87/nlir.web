fe8c48f7ebafcfed3bf43efc4ef5c816bf653cdf
AW-AlanWu <alan.92925@gmail.com>
2021-07-31 19:13:41 +0800

    check the user status in problem

--- clubSite/resource/views.py
+++ clubSite/resource/views.py
@@ -6,23 +6,22 @@ from django.urls import reverse
 
 def problem(request):
     context = {}
+    context['AC_list'] = Problem.objects.filter(result = "AC")
+    context['NA_list'] = Problem.objects.filter(result = "NA")
+    context['WA_list'] = Problem.objects.filter(result = "WA")
+    context['TLE_list'] = Problem.objects.filter(result = "TLE")
+    context['MLE_list'] = Problem.objects.filter(result = "MLE")
+    context['OLE_list'] = Problem.objects.filter(result = "OLE")
+    context['RE_list'] = Problem.objects.filter(result = "RE")
+    context['RF_list'] = Problem.objects.filter(result = "RF")
+    context['CE_list'] = Problem.objects.filter(result = "CE")
+    context['SE_list'] = Problem.objects.filter(result = "SE")
     if request.user.is_authenticated:
         context['order_list'] = request.user.problem_set.order_by('-problem_order')
-
-        context['AC_list'] = Problem.objects.filter(result = "AC")
-        context['NA_list'] = Problem.objects.filter(result = "NA")
-        context['WA_list'] = Problem.objects.filter(result = "WA")
-        context['TLE_list'] = Problem.objects.filter(result = "TLE")
-        context['MLE_list'] = Problem.objects.filter(result = "MLE")
-        context['OLE_list'] = Problem.objects.filter(result = "OLE")
-        context['RE_list'] = Problem.objects.filter(result = "RE")
-        context['RF_list'] = Problem.objects.filter(result = "RF")
-        context['CE_list'] = Problem.objects.filter(result = "CE")
-        context['SE_list'] = Problem.objects.filter(result = "SE")
-
         context['is_authenticated'] = True
+        return render(request, 'resource/problem-login.html', context=context)
 
-    return render(request, 'resource/problem.html', context=context)
+    return render(request, 'resource/problem-logout.html', context=context)
 
 def updateResult(request):
     if request.POST['result'] and request.POST['problem_order']:
